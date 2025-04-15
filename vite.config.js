bdaa619878cca6a0ad5db5c4e5369593fb964fdc
ankityadav456 <ankit.y.2302@gmail.com>
2025-04-15 21:14:19 +0530

    Updated changes to portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My_Portfolio/',   // Add this line to set base path for GitHub Pages
   plugins: [react()],
 })
